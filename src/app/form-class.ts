afd348e9551733d68120e71aa9c3613834c685d8
acmarshall <ambercarriemarshall@gmail.com>
2018-07-12 23:34:08 -0400

    Created a simple form that displays in console

---
+++ src/app/form-class.ts
@@ -0,0 +1,9 @@
+export class FormClass {
+
+constructor (
+	public name: string, 
+	public number: string, 
+	public email: string, 
+	public state: string
+) {}
+}
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import {FormClass} from '../form-class';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+state = ['', 'Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+model = new FormClass('','', '', this.state[0]); 
+
+submitted: boolean =false; 
+
+onSubmit() {
+this.submitted = true;
+console.log(this.model); 
+this.model = new FormClass('','', '', this.state[0]) ;
+ }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
